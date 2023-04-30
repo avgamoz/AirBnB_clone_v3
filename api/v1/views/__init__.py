a70a9bdd6f710bfdfba70ba184c803114c747cb7
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2023-04-30 18:04:27 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""The Blueprint for API """
+"""
+The Blueprint for API 
+"""
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
